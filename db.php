a05a12eaabeb2a2a4a8c7fea7f6347e5b658b906
Ilya <barilmail@rambler.ru>
2019-04-17 11:58:34 +0300

    fixed charset setting in db connection file

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $db_pass = 'baRil93;';
 $db_name = 'news_feed';
 
 $mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
-$mysqli->set_charset('utf-8');
+$mysqli->set_charset('utf8');
 
 if ($mysqli->connect_errno) {
     echo "Не удалось подключиться к базе данных: " . $mysqli->connect_error;
